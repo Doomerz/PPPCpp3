1e84e896060dd918161bb61261ebf4f1d25c3074
Doomerz <Doomerz@comcast.net>
2025-01-10 16:00:17 -0500

    c5 reading

--- Chapter5/main.cpp
+++ Chapter5/main.cpp
@@ -1,10 +1,18 @@
 import std;
 using namespace std;
 
-void TryThis3() {
+void TryThis3_4() {
+	//first validate expressions syntactic accuracy
+	//we could make a vector of these tokens perhaps and a token that represents a sub-expression
+	//make a first pass on the vector of tokens that replaces tokens enclosed in paranthesis as a sub-expression
+	//then separate the vector and vectors within the sub-expression tokens so that highest order of operation items are evaluated
+	//proceed with further and further lower order operations, several passes might be required.
+	//this may not be the most efficient solution though.
+}
+void TryThis6() {
 	//
 }
-void TryThis() {} //resume 5.2.1 @194
+void TryThis() {} //resume 5.4.2 @212
 
 ///Drill
 
@@ -13,6 +21,6 @@ void TryThis() {} //resume 5.2.1 @194
 ///Exercises
 
 int main() {
-	TryThis3();
+	TryThis3_4();
 	return 0;
 }
