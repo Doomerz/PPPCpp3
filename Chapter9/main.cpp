d28a0b333eb4c5d4f942a8a32cbdfccdc0b1fd34
Doomerz <Doomerz@comcast.net>
2025-03-21 07:07:22 -0400

    prep c9tt

--- Chapter9/main.cpp
+++ Chapter9/main.cpp
@@ -2,6 +2,13 @@ import std;
 using namespace std;
 
 ///TryThis
+void TryThis10_1() {
+	//output your birth year in decimal, hexadecimal, and octal form. Label each value
+	//line up output in columns using the tab character, output age now too.
+}
+void TryThis10_6() {
+	//see what that last statement prints and explain it. try some other formats.
+}
 
 ///Drill
 
@@ -11,7 +18,7 @@ using namespace std;
 
 ///Main
 int main() try {
-	placeholder();
+	TryThis10_1();
 	return 0;
 }
 catch (exception& e) {
